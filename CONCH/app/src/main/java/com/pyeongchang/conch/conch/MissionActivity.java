3e2d3014562212b688ba67c53ec716beac205054
Khyojung <918fall@gmail.com>
2017-07-13 14:04:50 +0900

    writing and timeline

--- CONCH/app/src/main/java/com/pyeongchang/conch/conch/MissionActivity.java
+++ CONCH/app/src/main/java/com/pyeongchang/conch/conch/MissionActivity.java
@@ -1,13 +1,19 @@
 package com.pyeongchang.conch.conch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MissionActivity extends AppCompatActivity {
 
+    DBAdapter db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
+
+        db = new DBAdapter(this);
+        Intent intent = new Intent(this, WritingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ CONCH/app/src/main/java/com/pyeongchang/conch/conch/TimeLineActivity.java
@@ -0,0 +1,50 @@
+package com.pyeongchang.conch.conch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class TimeLineActivity extends Activity{
+
+    TextView text;
+    CustomAdapter cAdapter;
+    DBAdapter dbAdapter;
+    PostDBAdapter postDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_line);
+
+        ListView lv = (ListView)findViewById(R.id.listView1);
+
+        Intent intent = getIntent();
+        String post = intent.getStringExtra("newWrite");
+
+        final ArrayList<Item> list = new ArrayList<Item>();
+        list.add(new Item(post));
+
+        cAdapter = new CustomAdapter(list, this);
+        lv.setAdapter(cAdapter);
+
+        dbAdapter = new DBAdapter(this);
+        dbAdapter.open();
+        postDB = new PostDBAdapter(this);
+        postDB.open();
+        ArrayList<String> array_list = (ArrayList) postDB.getAllPost();
+        TextView tv = findViewById(R.id.content);
+        tv.setText(array_list.get(0));
+    }
+
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, WritingActivity.class);
+        startActivity(intent);
+    }
+}
