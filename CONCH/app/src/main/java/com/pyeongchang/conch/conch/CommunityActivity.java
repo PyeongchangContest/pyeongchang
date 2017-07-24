b06bfafad2707a3cb79b2ce2925e39392ea426d5
Gayeon <victoria1559@naver.com>
2017-07-24 14:49:31 +0900

    make community activiry

--- CONCH/app/src/main/java/com/pyeongchang/conch/conch/CommunityActivity.java
+++ CONCH/app/src/main/java/com/pyeongchang/conch/conch/CommunityActivity.java
@@ -1,8 +1,18 @@
 package com.pyeongchang.conch.conch;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
- * Created by Gayeon on 2017-07-19.
+ * Created by Gayeon on 2017-07-24.
  */
 
-public class CommunityActivity {
+public class CommunityActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+        
+    }
 }
